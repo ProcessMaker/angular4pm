1ed9f8aaf53e5cc4f057cd4f2dc19b7eedb33cea
ethnp <1320941+ethnp@users.noreply.github.com>
2023-08-17 15:13:58 -0400

    added environment file handling and nav refactoring

--- src/app/components/nav/navigation.component.ts
+++ src/app/components/nav/navigation.component.ts
@@ -9,6 +9,7 @@ import { DbService } from 'src/app/services/db.service';
 export class NavigationComponent {
 	constructor(public authService: AuthService, public db: DbService) {}
 
+	// Method to log out the user by calling the logout method from the AuthService
 	logout() {
 		this.authService.logout();
 	}
