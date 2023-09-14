6b82cbbccf6faf913ca9b79ae43d1436cdd0c8d4
ethnp <1320941+ethnp@users.noreply.github.com>
2023-09-14 15:22:14 -0400

    removed pagination module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import '@angular/compiler';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './components/app.component';
 
@@ -12,7 +11,6 @@ import { AppComponent } from './components/app.component';
 	imports: [
 		BrowserModule,
 		HttpClientModule,
-		NgxPaginationModule,
 		FormsModule,
 		ReactiveFormsModule,
 		CommonModule,
